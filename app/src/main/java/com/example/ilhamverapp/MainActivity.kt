8db09f2f5277f1e98fe756c56824a218bbd84139
Bekir Geriş <bekir.geris@gmail.com>
2023-06-24 15:47:22 +0300

    compose öğrenmeye devam

--- app/src/main/java/com/example/ilhamverapp/MainActivity.kt
+++ app/src/main/java/com/example/ilhamverapp/MainActivity.kt
@@ -2,13 +2,26 @@ package com.example.ilhamverapp
 
 import android.annotation.SuppressLint
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.ilhamverapp.ui.theme.IlhamVerAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -34,11 +47,40 @@ fun Page() {
     Scaffold(
         topBar = {
             TopAppBar(
-                title = { Text(text = "İlham Ver") }
+                title = { Text(text = "İlham Ver") },
+                backgroundColor = colorResource(id = R.color.primaryColorDark),
+                contentColor = colorResource(id = R.color.white)
             )
         },
         content = {
-
+            Column(
+                modifier = Modifier.fillMaxSize(),
+                verticalArrangement = Arrangement.SpaceEvenly,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Column(horizontalAlignment = Alignment.CenterHorizontally) {
+                    Image(painter = painterResource(id = R.drawable.img), contentDescription = "")
+                    Text(
+                        text = "Begers Company",
+                        color = Color.Red,
+                        fontWeight = FontWeight.Bold,
+                        fontSize = 22.sp
+                    )
+                }
+                Text(
+                    text = "feadvb aelrbgvls aedlbrnv aeskb vaekb nlaen ael fnldnsf aldaflmdal fnmdaf nladl lsedqa ladfl nladsfl",
+                    modifier = Modifier.padding(all = 10.dp),
+                    textAlign = TextAlign.Center
+                )
+                Button(onClick = {
+                    Log.d("bekbek", "İlham ver butonuna tıklandı")
+                }, colors = ButtonDefaults.buttonColors(
+                    backgroundColor = Color.Red,
+                    contentColor = Color.White
+                )) {
+                    Text(text = "İlham Ver")
+                }
+            }
         }
     )
 }
